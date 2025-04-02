671574c6985849984a4a88a9eb4f30412ca8d8f6
MoaRosvallMT24 <moa.rosvall@iths.se>
2025-04-02 23:22:40 +0200

    Working on issues

--- src/test/java/se/iths/RunTest.java
+++ src/test/java/se/iths/RunTest.java
@@ -2,13 +2,13 @@ package se.iths;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import se.iths.*;
 
 public class RunTest {
-
+   
     @Test
     public void testNewRun() {
-        //vill testa skapa ny Run med alla variabler 
         Run cut = new Run("Test Run", 0.0, 0, "June15");
         assertEquals("Test Run", cut.getId());
-    }    
+    }   
 }
