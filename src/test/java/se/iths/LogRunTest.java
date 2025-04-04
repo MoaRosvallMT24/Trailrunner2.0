f6e0abd68b080c7e35983ee4b1e314bfeaad6c9e
MoaRosvallMT24 <moa.rosvall@iths.se>
2025-04-04 16:35:28 +0200

    Setting up the LogRun-files

---
+++ src/test/java/se/iths/LogRunTest.java
@@ -0,0 +1,14 @@
+package se.iths;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import se.iths.RunTest;
+
+public class LogRunTest {
+
+    @Test
+    public void testLogRun() {
+        
+    }
+    
+}
